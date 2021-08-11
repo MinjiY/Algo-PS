24b0858a76f0d578251af5f39ef0d310530d3f1e
youminji <upi907@naver.com>
2021-08-11 13:56:45 +0900

    [BOJ] 15657 N과 M(8)

---
+++ BOJ/brute_force/N과M/BOJ_15657.java
@@ -0,0 +1,49 @@
+package brute_force.N과M;
+/*
+    [BOJ] 15657 N과 M(8)
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15657 {
+    static int N,M;
+    static int selected[];
+    static int arr[];
+    static StringBuilder sb = new StringBuilder();
+
+    static int stoi(String token){
+        return Integer.parseInt(token);
+    }
+    static void solve(int indx,int cnt){
+        if(cnt == M){
+            for(int i=0; i<M; ++i){
+                sb.append(arr[selected[i]]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=indx; i<N; ++i){
+            selected[cnt] = i;
+            solve(i,cnt+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+
+        arr = new int[N];
+        selected = new int[M+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; ++i){
+            arr[i] = stoi(st.nextToken());
+        }
+        Arrays.sort(arr);
+        solve(0,0);
+        System.out.println(sb);
+    }
+}
